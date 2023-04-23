8919b3b6c1acafe9721463692cb419ac36bc80fa
daboyspace <daboyspace@gmail.com>
2023-04-23 17:08:23 +0300

    AddToysStore

---
+++ ToyStore/ToysStore.java
@@ -0,0 +1,34 @@
+package ToyStore;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class ToysStore{
+    private List<Toy> toys;
+
+    public ToysStore(List<Toy> toys) {
+        this.toys = toys;
+    }
+
+    public Toy getToyForPrice() {
+        RandomToyChooser random = new RandomToyChooser();
+        Toy toy = random.chooseOnWeight(toys);
+        return toy;
+    }
+
+    public void saveToyForLottery() {
+        Toy toy = getToyForPrice();
+        String text = toy.toString();
+        try(FileWriter writer = new FileWriter("Toys.txt", true))
+        {
+            writer.write(text);
+            writer.append('\n');
+            writer.flush();
+        }
+        catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+        toys.remove(toy);
+    }
+}
